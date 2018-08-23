258e5a0af5f7fba2420de1d572dac5a685ebe6e3
tianke.lyq <tianke.lyq@alibaba-inc.com>
2018-08-23 10:09:40 +0800

    add second recursion method to 16

--- 016_median_arrays.py
+++ 016_median_arrays.py
@@ -44,8 +44,40 @@ class Solution:
 
                 return (max_of_left + min_of_right) / 2.0
 
+    def findMedianSortedArrays2(self, A, B):
+        l = len(A) + len(B)
+        if l % 2 == 1:
+            return self.kth(A, B, l // 2)
+        else:
+            return (self.kth(A, B, l // 2) + self.kth(A, B, l // 2 - 1)) / 2.
+
+    def kth(self, a, b, k):
+        if not a:
+            return b[k]
+        if not b:
+            return a[k]
+        ia, ib = len(a) // 2, len(b) // 2
+        ma, mb = a[ia], b[ib]
+
+        # when k is bigger than the sum of a and b's median indices
+        if ia + ib < k:
+            # if a's median is bigger than b's, b's first half doesn't include k
+            if ma > mb:
+                return self.kth(a, b[ib + 1:], k - ib - 1)
+            else:
+                return self.kth(a[ia + 1:], b, k - ia - 1)
+        # when k is smaller than the sum of a and b's indices
+        else:
+            # if a's median is bigger than b's, a's second half doesn't include k
+            if ma > mb:
+                return self.kth(a[:ia], b, k)
+            else:
+                return self.kth(a, b[:ib], k)
+
 
 s = Solution()
-print(s.findMedianSortedArrays([1, 2, 3, 4, 5, 6], [4, 5]))
-print(s.findMedianSortedArrays([1, 2, 3, 4, 5], [7]))
-print(s.findMedianSortedArrays([], [7]))
+# print(s.findMedianSortedArrays([1, 2, 3, 4, 5, 6], [4, 5]))
+# print(s.findMedianSortedArrays([1, 2, 3, 4, 5], [7]))
+# print(s.findMedianSortedArrays([], [7]))
+
+print(s.findMedianSortedArrays2([1, 2, 3, 4, 5], [7]))
