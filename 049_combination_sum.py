417939219cc2fb006dcf84ee9a6738dd3090da73
tianke.lyq <tianke.lyq@alibaba-inc.com>
2018-08-25 20:44:58 +0800

    add 49

---
+++ 049_combination_sum.py
@@ -0,0 +1,29 @@
+class Solution:
+    def combinationSum(self, candidates, target):
+        """
+        :type candidates: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        def dfs(remain, combo, index):
+            if remain == 0:
+                result.append(combo)
+                return
+            for i in range(index, len(candy)):
+                if i< len(candy) -1 and candy[i] == candy[i+1]:
+                    continue
+                if candy[i] > remain:
+                    # exceeded the sum with candidate[i]
+                    break #the for loop
+                
+                dfs(remain - candy[i], combo + [candy[i]], i)
+                
+        candy = sorted(candidates)
+        result = []
+        dfs(target, [], 0)
+        return result
+
+
+s = Solution()
+print(s.combinationSum([2, 3, 6, 7], 7))
+print(s.combinationSum([8, 10, 6, 11, 1, 16, 8], 28))
