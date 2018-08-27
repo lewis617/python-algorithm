83aab2d535e57ef0fdc73189f3c4dc21ba8ab60f
tianke.lyq <tianke.lyq@alibaba-inc.com>
2018-08-27 14:55:15 +0800

    add 63

---
+++ src/063_copy_list.py
@@ -0,0 +1,21 @@
+# Definition for singly-linked list with a random pointer.
+class RandomListNode:
+    def __init__(self, x):
+        self.label = x
+        self.next = None
+        self.random = None
+
+class Solution:
+    # @param head, a RandomListNode
+    # @return a RandomListNode
+    def copyRandomList(self, head):
+        dic = dict()
+        m = n = head
+        while m:
+            dic[m] = RandomListNode(m.label)
+            m = m.next
+        while n:
+            dic[n].next = dic.get(n.next)
+            dic[n].random = dic.get(n.random)
+            n = n.next
+        return dic.get(head)
