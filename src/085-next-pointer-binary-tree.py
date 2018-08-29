e9cdbc4cec6afacaf404b8aaf366c66acb2f29d3
lewis617 <lewis617@163.com>
2018-08-30 00:57:17 +0800

    add 85

---
+++ src/085-next-pointer-binary-tree.py
@@ -0,0 +1,22 @@
+# Definition for a  binary tree node
+# class TreeLinkNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+#         self.next = None
+
+class Solution:
+    # @param root, a tree node
+    # @return nothing
+    def connect(self, root):
+        if not root:
+            return
+        if not root.left and not root.right:
+            return
+        root.left.next = root.right
+        if root.next:
+            root.right.next = root.next.left
+        self.connect(root.left)
+        self.connect(root.right)
+        
