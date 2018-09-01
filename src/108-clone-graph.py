4d5e3b9f4ca35bed2d351cf58465d6216e04943a
tianke.lyq <tianke.lyq@alibaba-inc.com>
2018-09-01 12:41:07 +0800

    update 106

--- src/108-clone-graph.py
+++ src/108-clone-graph.py
@@ -1,8 +1,8 @@
 # Definition for a undirected graph node
-# class UndirectedGraphNode:
-#     def __init__(self, x):
-#         self.label = x
-#         self.neighbors = []
+class UndirectedGraphNode:
+    def __init__(self, x):
+        self.label = x
+        self.neighbors = []
 
 class Solution:
     # @param node, a undirected graph node
