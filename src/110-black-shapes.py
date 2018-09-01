9367ed664bd727cd420ae3c88efaad256c150671
tianke.lyq <tianke.lyq@alibaba-inc.com>
2018-09-01 15:57:18 +0800

    update 110

--- src/110-black-shapes.py
+++ src/110-black-shapes.py
@@ -4,19 +4,19 @@ class Solution:
     def black(self, A):
         A = [list(s) for s in A]
 
-        def bfs(x, y):
+        def dfs(x, y):
             if not 0 <= x < len(A) or not 0 <= y < len(A[0]) or A[x][y] != 'X':
                 return
             A[x][y] = 'Y'
-            bfs(x, y+1)
-            bfs(x, y-1)
-            bfs(x+1, y)
-            bfs(x-1, y)
+            dfs(x, y+1)
+            dfs(x, y-1)
+            dfs(x+1, y)
+            dfs(x-1, y)
         ans = 0
         for i in range(len(A)):
             for j in range(len(A[0])):
                 if A[i][j] == 'X':
-                    bfs(i, j)
+                    dfs(i, j)
                     ans += 1
         return ans
 
