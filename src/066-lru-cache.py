395d1c3c148cc8ef675fbb744def7f8e8e40b39d
tianke.lyq <tianke.lyq@alibaba-inc.com>
2018-09-09 15:22:41 +0800

    update 66

--- src/066-lru-cache.py
+++ src/066-lru-cache.py
@@ -1,57 +1,63 @@
-class Node:
-    def __init__(self, k, v):
-        self.key = k
-        self.val = v
+class Node():
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
         self.prev = None
         self.next = None
-class LRUCache:
+        
+class LRUCache(object):
 
     def __init__(self, capacity):
+        """
+        :type capacity: int
+        """
         self.capacity = capacity
-        self.dic = dict()
+        self.dict = {}
         self.head = Node(0, 0)
         self.tail = Node(0, 0)
-        self.head.next = self.tail
-        self.tail.prev = self.head
-
+        self.head.next, self.tail.prev = self.tail, self.head
+        
     def get(self, key):
-        if key in self.dic:
-            n = self.dic[key]
+        """
+        :type key: int
+        :rtype: int
+        """
+        if key in self.dict:
+            n = self.dict[key]
             self._remove(n)
             self._add(n)
-            return n.val
+            return n.value
         return -1
+        
 
-    def set(self, key, value):
-        if key in self.dic:
-            self._remove(self.dic[key])
+    def put(self, key, value):
+        """
+        :type key: int
+        :type value: int
+        :rtype: void
+        """
+        if key in self.dict:
+            self._remove(self.dict[key])
         n = Node(key, value)
+        self.dict[key] = n
         self._add(n)
-        self.dic[key] = n
-        if len(self.dic) > self.capacity:
+        if len(self.dict) > self.capacity:
             n = self.head.next
             self._remove(n)
-            del self.dic[n.key]
-
-    def _remove(self, node):
-        p = node.prev
-        n = node.next
-        p.next = n
-        n.prev = p
-
-    def _add(self, node):
-        p = self.tail.prev
-        p.next = node
-        node.prev = p
-        self.tail.prev = node
-        node.next = self.tail
+            del self.dict[n.key]
+            
+    def _remove(self, n):
+        n.prev.next, n.next.prev = n.next, n.prev
+        
+    def _add(self, n):
+        self.tail.prev.next, self.tail.prev, n.prev, n.next = n, n, self.tail.prev, self.tail
 
 
 s = LRUCache(2)
-s.set(1, 10)
-s.set(2, 15)
+s.put(1, 10)
+s.put(2, 15)
 print(s.get(1))
 print(s.get(2))
 print(s.get(10))
-s.set(6, 14)
-print(s.get(2))
+s.put(6, 14)
+print(s.get(1))
