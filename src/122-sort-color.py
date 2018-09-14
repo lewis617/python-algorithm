94224e16e8ba068befe92031e3e5541b42fe8c10
tianke.lyq <tianke.lyq@alibaba-inc.com>
2018-09-14 16:52:27 +0800

    add 122

---
+++ src/122-sort-color.py
@@ -0,0 +1,13 @@
+def sortColors(nums):
+    i = j = 0
+    for k in xrange(len(nums)):
+        v = nums[k]
+        nums[k] = 2
+        if v < 2:
+            nums[j] = 1
+            j += 1
+        if v == 0:
+            nums[i] = 0
+            i += 1
+    return nums
+print(sortColors([2,0,2,1,1,0]))
