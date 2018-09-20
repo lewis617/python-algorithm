4a3dbef75a8e6e8b3ac25b40d2868ee09664d250
tianke.lyq <tianke.lyq@alibaba-inc.com>
2018-09-20 15:10:25 +0800

    update 99

--- src/099-palindrome-partitioning-ii.py
+++ src/099-palindrome-partitioning-ii.py
@@ -1,20 +1,28 @@
-class Solution:
-    # @param A : string
-    # @return an integer
-    def minCut(self, A):
-        if A == A[::-1]:
+class Solution(object):
+    def minCut(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        if s == s[::-1]:
             return 0
-        for i in range(len(A)):
-            if A[:i] == A[:i][::-1] and A[i:] == A[i:][::-1]:
+        for i in range(1, len(s)+1):
+            if s[:i] == s[:i][::-1] and s[i:] == s[i:][::-1]:
                 return 1
-        cut = [i for i in range(-1, len(A))]
-        for i in range(len(A)):
-            r1 = r2 = 0
-            while i-r1>=0 and i+r1<len(A) and A[i-r1] == A[i+r1]:
-                cut[i+r1+1] = min(cut[i+r1+1], cut[i-r1]+1)
-                r1 += 1
-            while i-r2>=0 and i+1+r2 < len(A) and A[i-r2] == A[i+1+r2]:
-                cut[i+r2+2] = min(cut[i+r2+2], cut[i-r2]+1)
+        dp = range(-1, len(s))
+        for i in range(len(s)):
+            r1=r2=0
+            while i-r1>=0 and i+r1<len(s) and s[i-r1] == s[i+r1]:
+                dp[i+r1+1] = min(dp[i+r1+1], dp[i-r1]+1)
+                r1+=1
+            while i-r2>=0 and i+r2+1<len(s) and s[i-r2] == s[i+r2+1]:
+                dp[i+r2+2] = min(dp[i+r2+2], dp[i-r2]+1)
                 r2+=1
-        return cut[-1]
+        return dp[-1]
+        
+        
+        
+        
                 
+s = Solution()
+print(s.minCut('fdafdsagfdagffadsafsd'))
