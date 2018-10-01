e724ff0cc2d412da90e0d989490940d04a8316ba
tianke.lyq <tianke.lyq@alibaba-inc.com>
2018-10-01 15:13:59 +0800

    add 134

---
+++ src/134-queue-reconstruction-by-height.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def reconstructQueue(self, people):
+        if not people: return []
+
+        # obtain everyone's info
+        # key=height, value=k-value, index in original array
+        peopledct, height, res = {}, [], []
+        
+        for i in xrange(len(people)):
+            p = people[i]
+            if p[0] in peopledct:
+                peopledct[p[0]] += (p[1], i),
+            else:
+                peopledct[p[0]] = [(p[1], i)]
+                height += p[0],
+
+        height.sort()      # here are different heights we have
+
+        # sort from the tallest group
+        for h in height[::-1]:
+            peopledct[h].sort()
+            for p in peopledct[h]:
+                res.insert(p[0], people[p[1]])
+
+        return res
+
+s= Solution()
+print(s.reconstructQueue([[7,0], [4,4], [7,1], [5,0], [6,1], [5,2]]))
